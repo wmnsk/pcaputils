6e2a8ac078a3bc75cde7547516be6bd9ef7dcd54
Yoshiyuki Kurauchi <ahochauwaaaaa@gmail.com>
2017-11-02 19:12:28 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# PCAP Utilities
+
+Utilities to handle PCAPs, written in Golang.
+
+
+### Contents
+
+| Directory | Description |
+|---|---|
+| str2pcap | Convert Hex/Base64 encoded string to PCAP ~~and vice versa~~.|
+| others | coming soon? |
+
+
+### Getting started
+
+1. Install Go from [Official Site](https://golang.org/doc/install).
+2. Install the tools you want by `go get github.com/pcaputils/<tool>`.
+3. `cd` to `$GOPATH/src/github.com/wmnsk/pcaputils/<tool>`
+4. Build it by `go build <tool>.go` or try it by `go run <tool>.go`.
+
+
+
+*Need to run on other platforms? [Dave Cheney's instruction](https://dave.cheney.net/2015/03/03/cross-compilation-just-got-a-whole-lot-better-in-go-1-5) well explains how to cross-compile Go source.*
+
+### Author
+
+Yoshiyuki Kurauchi ([GitHub](https://github.com/wmnsk/) / [Twitter](https://twitter.com/wmnskdmms))
+
+### LICENSE
+
+[MIT](https://github.com/wmnsk/pcaputils/blob/master/LICENSE)
---
+++ str2pcap/str2pcap.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"flag"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcapgo"
+)
+
+// maybeBase64 guesses the string is Base64-formatted or not.
+// As you see, normal strings can also be concluded as Base64...
+// Using regexp package will do the better job, but it'll be tiresome much more :-P
+func maybeBase64(s string) bool {
+	gzUpper := "G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | W | X | Y | Z"
+	gzLower := "g | h | i | j | k | l | m | n | o | p | q | r | s | t | u | v | w | x | y | z"
+	if strings.ContainsAny(s, gzUpper) || strings.ContainsAny(s, gzLower) {
+		return true
+	}
+	return false
+}
+
+func main() {
+	var (
+		input  = flag.String("s", "", "Hex or Base64 encoded string.")
+		output = flag.String("o", "WOW.pcap", "Name of PCAP to export.")
+	)
+	flag.Parse()
+	if *input == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	// Convert string into binary.
+	var data []byte
+	var err error
+	if maybeBase64(*input) { // guess format b64 or hex.
+		data, err = base64.StdEncoding.DecodeString(*input)
+		if err != nil {
+			log.Println(err)
+		}
+	} else { // try hex.
+		data, err = hex.DecodeString(*input)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	// Unexpected. This shouldn't be called.
+	if data == nil {
+		log.Fatal("Hey, something went wrong ¯\\_(ツ)_/¯")
+	}
+
+	// Create file to be written.
+	f, err := os.Create(*output)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Setup PCAP writer.
+	w := pcapgo.NewWriter(f)
+	w.WriteFileHeader(65536, layers.LinkTypeEthernet)
+
+	// Write PCAP.
+	w.WritePacket(
+		gopacket.CaptureInfo{
+			Timestamp:      time.Now(),
+			CaptureLength:  len(data),
+			Length:         len(data),
+			InterfaceIndex: 0,
+		},
+		data,
+	)
+	f.Close()
+
+	log.Printf("Successfully exported PCAP as \"%s\"", *output)
+}
